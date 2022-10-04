bb3ae32ae84801d4b440dd56c79007d9a51b40a2
Admin <duydt.dn@gmail.com>
2022-10-04 15:35:41 +0700

    [Proj] update Procfile

--- AlphaFusionVN/settings.py
+++ AlphaFusionVN/settings.py
@@ -15,6 +15,7 @@ from pathlib import Path
 gettext = lambda s: s
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 # BASE_DIR = Path(__file__).resolve().parent.parent
+DATA_DIR = os.path.dirname(os.path.dirname(__file__))
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
@@ -216,6 +217,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
--- Procfile
+++ Procfile
@@ -1,1 +1,4 @@
 "web: gunicorn AlphaFusionVN.wsgi --log-file -" 
+python manage.py collectstatic --noinput
+manage.py migrate
+release: python manage.py migrate
