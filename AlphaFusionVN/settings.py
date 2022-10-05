61312f0b25bc00fbb139e88332ad874098fe9212
Admin <duydt.dn@gmail.com>
2022-10-05 09:43:51 +0700

    [Proj] update Procfile

--- AlphaFusionVN/settings.py
+++ AlphaFusionVN/settings.py
@@ -138,7 +138,7 @@ MIDDLEWARE = [
     'cms.middleware.toolbar.ToolbarMiddleware',
     'cms.middleware.language.LanguageCookieMiddleware'
 ]
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'AlphaFusionVN.urls'
 
 TEMPLATES = [
@@ -216,14 +216,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-MEDIA_URL = '/media/'
 
 USE_THOUSAND_SEPARATOR = True
 
@@ -254,15 +247,27 @@ CKEDITOR_CONFIGS = {
         ),
     }
 }
-django_heroku.settings(locals(), staticfiles=False)
-AWS_ACCESS_KEY_ID = 'AKIAVQMSLKSCP3ZQYM7R'
-AWS_SECRET_ACCESS_KEY = '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'
-DEFAULT_FILE_STORAGE = 'AFWeb.storage_backends.MediaStorage'
-AWS_STORAGE_BUCKET_NAME = 'afv-assets'
-AWS_S3_REGION_NAME = 'us-east-1'
-AWS_URL='https://afv-assets.s3.amazonaws.com/'
-AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
-AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
-STATIC_URL = AWS_URL + '/static/'
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-MEDIA_URL = AWS_URL + '/media/'
+USE_S3 = os.getenv('USE_S3') == 'FALSE'
+if USE_S3:
+    django_heroku.settings(locals(), staticfiles=False)
+    AWS_ACCESS_KEY_ID = 'AKIAVQMSLKSCP3ZQYM7R'
+    AWS_SECRET_ACCESS_KEY = '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'
+    DEFAULT_FILE_STORAGE = 'AFWeb.storage_backends.MediaStorage'
+    AWS_STORAGE_BUCKET_NAME = 'afv-assets'
+    AWS_S3_REGION_NAME = 'us-east-1'
+    AWS_URL = 'https://afv-assets.s3.amazonaws.com/'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
+    AWS_S3_OBJECT_PARAMETERS = {'CacheControl': 'max-age=86400'}
+    STATIC_URL = AWS_URL + '/staticfiles/'
+    STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+    MEDIA_URL = AWS_URL + '/media/'
+else:
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATIC_URL = '/static/'
+    STATICFILES_DIRS = [
+        os.path.join(BASE_DIR, "static"),
+    ]
+    MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+    MEDIA_URL = '/media/'
+
