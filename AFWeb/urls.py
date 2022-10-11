af28bc69ffe17f9bba904fc8ff104dd5a370148a
Admin <duydt.dn@gmail.com>
2022-10-11 16:55:34 +0700

    [add] something modules

--- AFWeb/urls.py
+++ AFWeb/urls.py
@@ -7,7 +7,7 @@ app_name = "AFWeb"
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^en/$', views.index, name='news_list'),
-    url(r'^(?P<category_slug>[-\w]+)/$', views.get_news_by_category, name='get-news-by-category'),
+    # url(r'^(?P<category_slug>[-\w]+)/$', views.get_news_by_category, name='get-news-by-category'),
     path('news/detail/<str:slug>......<int:id>/', views.news_detail_view, name='news_detail_view'),
     path('news/search/', views.SearchResultsView.as_view(), name='rq-search-by-keyword'),
 ]
